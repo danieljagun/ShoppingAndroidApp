5a12f96e3b1ad16305f6d399ccd18192a0efb80f
danieljagun <114102084+danieljagun@users.noreply.github.com>
2023-10-17 21:40:00 +0100

    cleaned up code (#2)
    Co-authored-by: Daniel J <daniel@Daniels-MacBook-Air-2.local>

--- app/src/main/java/com/example/shoppingapp/SignUpActivity.java
+++ app/src/main/java/com/example/shoppingapp/SignUpActivity.java
@@ -1,16 +1,15 @@
 package com.example.shoppingapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -64,7 +63,6 @@ public class SignUpActivity extends AppCompatActivity {
                 }
             }
         });
-
 
         loginRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
